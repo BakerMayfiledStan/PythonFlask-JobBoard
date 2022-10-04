8108c60514cdc77e271042f12d4daaf849d50463
BakerMayfiledStan <108005734+BakerMayfiledStan@users.noreply.github.com>
2022-10-04 12:39:58 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/jobs')
 def jobs():
-	return render_template(
-		'index.html', route('/'), route('/jobs')
-	)
+	return render_template('index.html')
